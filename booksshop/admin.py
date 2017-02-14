de84cf977e7b2e2104de2f519b0281ab571f9550
mmishak <mmishak.ya@gmail.com>
2017-02-15 00:07:21 +0300

    Add __str__ function to models

--- booksshop/admin.py
+++ booksshop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from booksshop.models import *
+
+admin.site.register(PublishHouse)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Reward)
+admin.site.register(Provider)
+admin.site.register(Stock)
+admin.site.register(Client)
+admin.site.register(Book)
+admin.site.register(Order)
+admin.site.register(Review)
+admin.site.register(OrderList)
+admin.site.register(Supply)
+admin.site.register(StockComposition)
+admin.site.register(SupplyComposition)
--- booksshop/models.py
+++ booksshop/models.py
@@ -1,30 +1,49 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class PublishHouse(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Reward(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Provider(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Stock(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Client(models.Model):
@@ -32,6 +51,9 @@ class Client(models.Model):
     email = models.EmailField
     password = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -46,13 +68,19 @@ class Book(models.Model):
     categories = models.ManyToManyField(Category)
     rewards = models.ManyToManyField(Reward)
 
+    def __str__(self):
+        return self.title
+
 
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)  # надо бы перенети в OrderList
     price = models.FloatField
     date = models.DateField
     recommendations = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return 'Order by client: ' + Client.objects.get(id=self.client).name
 
 
 class Review(models.Model):
@@ -61,6 +89,10 @@ class Review(models.Model):
     text = models.CharField(max_length=400)
     date = models.DateField
     rating = models.PositiveIntegerField
+
+    def __str__(self):
+        return 'Review by client ' + Client.objects.get(id=self.client).name + ' to book ' + Book.objects.get(
+            id=self.client)
 
 
 class OrderList(models.Model):
